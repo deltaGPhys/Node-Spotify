80008aa43e670cd1bd5d89da99a1493af3f8617f
deltagphys <cty.jgates@gmail.com>
2019-12-10 22:15:11 -0500

    updating repo

--- routes/index.js
+++ routes/index.js
@@ -18,12 +18,33 @@ router.get('/', function (req, res) {
   res.render('index', {title: 'Spotify', results: results});
 });
 
+/* GET home page. */
+router.post('/tracks', function (req, res) {
+    var query = req.body.artist;
+    var artist = req.body.artistName;
+
+    results = [];
+    spotify.request('https://api.spotify.com/v1/artists/'+query+'/top-tracks?country=US')
+        .then(function(spotRes) {
+
+            //Store the artist, song, preview link, and album in the results array
+            spotRes.tracks.forEach(function(ea){
+                results.push({
+                    song: ea.name,
+                    trackId: ea.id,
+                    album: ea.album.name});
+            });
+            //Render the homepage and return results to the view
+            res.render('tracks', {artist: artist, results: results});
+        })
+        .catch(function(err) {
+            console.error('Error occurred: ' + err);
+        });
+});
 
 router.post('/namecheck', function (req, res) {
     //Get the query from the user
     var query = req.body.partial;
-    console.log(query);
-
 
     //Clear out old results
     results = [];
@@ -43,18 +64,18 @@ router.post('/namecheck', function (req, res) {
                     var topTrack = spotRes.tracks.items[0].name;
                     var topTrackID = spotRes.tracks.items[0].id;
                     res.setHeader('Content-Type', 'application/json');
-                    res.end(JSON.stringify({ topArtist: artist, albumArt: albumArt, topTrack: topTrack, topTrackID: topTrackID }));
+                    res.end(JSON.stringify({ topArtist: artist, artistId: artistId, albumArt: albumArt, topTrack: topTrack, topTrackID: topTrackID }));
 
                 })
                 .catch(function (err) {
                 console.log(err);
-                res.end(JSON.stringify({ topArtist: artist, albumArt: albumArt, topTrack: null, topTrackID: null }));
+                res.end(JSON.stringify({ topArtist: artist, artistId: artistId, albumArt: albumArt, topTrack: null, topTrackID: null }));
             });
 
         })
         .catch(function (err) {
             console.log(err);
-            res.end(JSON.stringify({ topArtist: null, albumArt: null, topTrack: null, topTrackID: null }));
+            res.end(JSON.stringify({ topArtist: null, artistId: null, albumArt: null, topTrack: null, topTrackID: null }));
         });
 });
 
